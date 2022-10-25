a081a5a9ec9e70f5daa93f8b39a776365d0ce9b4
Jonathan <jonathanallsop2@gmail.com>
2022-10-25 14:08:32 +0100

    Checks the input

--- milestone_2.py
+++ milestone_2.py
@@ -3,6 +3,11 @@ import random
 word_list = ['Apple','Banana','Orange','Pear','Grape']
 word = random.choice(word_list)
 
+alphabet = ['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+
 guess = input("Please enter your guess: ")
 
-print(guess)
+if len(guess) == 1 and guess in alphabet:
+    print('Good Guess!')
+else:
+    print('Oops! That is not a valid input.')
