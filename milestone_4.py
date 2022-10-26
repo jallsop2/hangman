fd6bebd5379993853a89c58948dad400be8b6638
Jonathan <jonathanallsop2@gmail.com>
2022-10-26 16:57:58 +0100

    Adds methods to the hangman class

--- milestone_4.py
+++ milestone_4.py
@@ -1,11 +1,12 @@
 import random
 
-word_list = ['Apple','Banana','Orange','Pear','Grape']
+word_list = ['apple','banana','orange','pear','grape']
 word = random.choice(word_list)
 
 class Hangman():
     def __init__(self,word_list,num_lives = 5):
         self.word = random.choice(word_list)
+        print(self.word)
 
         self.word_guessed = ['_']*len(self.word)
         
@@ -17,25 +18,24 @@ class Hangman():
 
         self.list_of_guesses = []
 
-
-
-
-def check_guess(guess):
-    guess = guess.lower()
-    if guess in word:
-        print(f'Good guess! {guess} is in the word!')
-    else:
-        print(f'Sorry, {guess} is not in the word. Try again.')
-
-def ask_for_input():
-    while True:
-        guess = input("Please enter your guess: ")
-
-        if len(guess) == 1 and guess.isalpha() == True:
-            break
+    def check_guess(self,guess):
+        guess = guess.lower()
+        if guess in self.word:
+            print(f'Good guess! {guess} is in the word!')
         else:
-            print('Invalid letter. Please enter a single alphabetical character.')
-    
-    check_guess(guess)
+            print(f'Sorry, {guess} is not in the word. Try again.')
 
-ask_for_input()
+    def ask_for_input(self):
+        while True:
+            guess = input("Please enter your guess: ")
+
+            if len(guess) != 1 or guess.isalpha() == False:
+                print('Invalid letter. Please enter a single alphabetical character.')
+            elif guess in self.list_of_guesses:
+                print('You already tried that letter!')
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                
+game = Hangman(word_list)
+game.ask_for_input()
