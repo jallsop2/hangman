2321f5d5c358345a6689d91446818406fdee2122
Jonathan <jonathanallsop2@gmail.com>
2022-10-26 17:55:04 +0100

    Defines what incorrect guesses do

--- milestone_4.py
+++ milestone_4.py
@@ -27,9 +27,10 @@ class Hangman():
                     self.word_guessed[i] = guess
             self.num_letters -= 1
             
-
         else:
-            print(f'Sorry, {guess} is not in the word. Try again.')
+            self.num_lives -= 1
+            print(f'Sorry, {guess} is not in the word.')
+            print(f'You have {self.num_lives} left.')
 
     def ask_for_input(self):
         while True:
