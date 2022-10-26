47e9f35780c5ef3509085e2957fd5ba8d8e854ef
Jonathan <jonathanallsop2@gmail.com>
2022-10-26 16:40:32 +0100

    Creates hangman class

--- milestone_4.py
+++ milestone_4.py
@@ -7,7 +7,7 @@ class Hangman():
     def __init__(self,word_list,num_lives = 5):
         self.word = random.choice(word_list)
 
-        self.word_guessed = ['']*len(self.word)
+        self.word_guessed = ['_']*len(self.word)
         
         self.num_letters = len(list(set(self.word)))
         
