847ff80730d8e4d62b43df4d1143fcae92cbd9c9
Jonathan <jonathanallsop2@gmail.com>
2022-10-26 13:29:45 +0100

    splits into functions

--- milestone_3.py
+++ milestone_3.py
@@ -5,14 +5,14 @@ word = random.choice(word_list)
 
 
 
-def check_guess(word,guess):
+def check_guess(guess):
     guess = guess.lower()
     if guess in word:
         print(f'Good guess! {guess} is in the word!')
     else:
         print(f'Sorry, {guess} is not in the word. Try again.')
 
-def ask_for_input(word):
+def ask_for_input():
     while True:
         guess = input("Please enter your guess: ")
 
@@ -21,6 +21,6 @@ def ask_for_input(word):
         else:
             print('Invalid letter. Please enter a single alphabetical character.')
     
-    check_guess(word,guess)
+    check_guess(guess)
 
-ask_for_input(word)
+ask_for_input()
