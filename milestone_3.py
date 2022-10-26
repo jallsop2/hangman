4ce0c58b84febe92592ed33c9c4849656123b5cb
Jonathan <jonathanallsop2@gmail.com>
2022-10-26 13:18:35 +0100

    Checks if the guess is correct

--- milestone_3.py
+++ milestone_3.py
@@ -1,3 +1,8 @@
+from multiprocessing.resource_sharer import stop
+import random
+
+word_list = ['Apple','Banana','Orange','Pear','Grape']
+word = random.choice(word_list)
 
 while True:
     guess = input("Please enter your guess: ")
@@ -6,3 +11,8 @@ while True:
      break
     else:
         print('Invalid letter. Please enter a single alphabetical character.')
+
+if guess in word:
+    print(f'Good guess! {guess} is in the word!')
+else:
+    print(f'Sorry, {guess} is not in the word. Try again.')
